d2746ab5dd2fe29ba094c6529183febc95c70e3b
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-06-09 17:15:08 +0200

    Comment in repl jvm opts in project.clj

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.suskalo/discljord "0.2.8"]]
-  ;:jvm-opts ["-Dclojure.server.repl=\"{:port 5555 :accept clojure.core.server/repl}\""]
+  :jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]
   :main poker.discord.bot
   :repl-options {:init-ns poker.logic.game})
